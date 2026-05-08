fa3a685661fdde0b4568050b81934c73afc22f92
kat <katwang7749@gmail.com>
2026-05-07 23:05:23 -0400

    add frontend stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,39 @@
-<div class="prose">
-	<img src="/75wlogo.png" alt="75wang logo" />
-	<h1 class="font-kisba">75wang</h1>
-	<h2 class="font-hina">a customizable 75 hard tracker</h2>
-	<!-- <input placeholder="enter goals here..." type="checkbox" /> -->
+<div class="h-screen w-screen place-items-center bg-linear-to-b from-white to-pink-50 p-12">
+	<div>
+		<img src="/75wlogo.png" alt="75wang logo" class="h-35 w-auto duration-300 hover:scale-102" />
+		<div class="m-4 flex flex-row gap-2">
+			<h1 class="font-kisba text-4xl text-pink-950 duration-200 hover:p-1"><i>/75wæŋ/:</i></h1>
+			<h2 class="m-2.5 font-kisba text-2xl text-pink-900 duration-200 hover:p-1">
+				<b>75wang -</b> a customizable 75 hard tracker
+			</h2>
+		</div>
 
-	<h3 class="font-hina">i made this solely for myself but why not i guess anyone can use it</h3>
+		<!-- <input placeholder="enter goals here..." type="checkbox" /> -->
+		<form id="task-form" class="flex flex-row p-2">
+			<input
+				type="text"
+				id="task-input"
+				placeholder="enter goals here..."
+				class="mb-4 field-sizing-content w-fit p-2 px-6 font-hina text-[22px] text-pink-950 duration-300 hover:p-3 focus:outline-none"
+				autocomplete="off"
+			/>
+			<button
+				type="submit"
+				class="mb-4 ml-4 px-4 font-hina text-[22px] font-bold text-pink-800 duration-300 hover:scale-110 hover:rotate-10"
+				>+</button
+			>
+		</form>
+
+		<h3 class="font-hina text-xl text-pink-950">
+			i made this solely for myself but why not i guess anyone can use it
+		</h3>
+	</div>
 </div>
+
+<h3 class="m-2 px-4 text-left font-hina text-xl text-pink-950">
+	made w/★ by kat wang
+	<br />75wang is open source (add link later)
+	<br /> last updated may 2026
+</h3>
+<!-- 
+        <button type="submit" class="px-4 rounded-md bg-linear-to-b from-pink-good to-pink-900/35 mb-4 pb-3 ml-4 text-3xl text-pink-950 font-bold font-hina">+</button> -->
