cd2abfe96473c6b6c653a09a15c2517767704636
kat <katwang7749@gmail.com>
2026-05-07 22:31:10 -0400

    add fonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="prose">
+	<img src="/75wlogo.png" alt="75wang logo" />
+	<h1 class="font-kisba">75wang</h1>
+	<h2 class="font-hina">a customizable 75 hard tracker</h2>
+	<!-- <input placeholder="enter goals here..." type="checkbox" /> -->
+
+	<h3 class="font-hina">i made this solely for myself but why not i guess anyone can use it</h3>
+</div>
