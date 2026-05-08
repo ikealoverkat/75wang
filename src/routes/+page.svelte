5059a6f991a252120a118d1d53d8c229405d4822
kat <katwang7749@gmail.com>
2026-05-07 23:22:24 -0400

    add to-do list function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,59 @@
-<div class="h-screen w-screen place-items-center bg-linear-to-b from-white to-pink-50 p-12">
-	<div>
-		<img src="/75wlogo.png" alt="75wang logo" class="h-35 w-auto duration-300 hover:scale-102" />
-		<div class="m-4 flex flex-row gap-2">
-			<h1 class="font-kisba text-4xl text-pink-950 duration-200 hover:p-1"><i>/75wæŋ/:</i></h1>
-			<h2 class="m-2.5 font-kisba text-2xl text-pink-900 duration-200 hover:p-1">
-				<b>75wang -</b> a customizable 75 hard tracker
-			</h2>
-		</div>
+<script>
+	import { text } from '@sveltejs/kit';
 
-		<!-- <input placeholder="enter goals here..." type="checkbox" /> -->
-		<form id="task-form" class="flex flex-row p-2">
-			<input
-				type="text"
-				id="task-input"
-				placeholder="enter goals here..."
-				class="mb-4 field-sizing-content w-fit p-2 px-6 font-hina text-[22px] text-pink-950 duration-300 hover:p-3 focus:outline-none"
-				autocomplete="off"
-			/>
-			<button
-				type="submit"
-				class="mb-4 ml-4 px-4 font-hina text-[22px] font-bold text-pink-800 duration-300 hover:scale-110 hover:rotate-10"
-				>+</button
-			>
-		</form>
+	let tasks = $state([]);
+	let newTask = $state('');
+	let InputElement = $state();
 
-		<h3 class="font-hina text-xl text-pink-950">
-			i made this solely for myself but why not i guess anyone can use it
-		</h3>
+	function addTask(e) {
+		if (e) e.preventDefault();
+
+		if (newTask.trim() !== '') {
+			tasks = [...tasks, { id: Date.now(), text: newTask, completed: false }];
+			newTask = '';
+		}
+		InputElement.focus();
+	}
+</script>
+
+<div
+	class="h-screen w-screen place-items-center overflow-x-hidden bg-linear-to-b from-white to-pink-50 p-12"
+>
+	<img src="/75wlogo.png" alt="75wang logo" class="h-35 w-auto duration-300 hover:scale-102" />
+	<div class="m-4 flex flex-row gap-2">
+		<h1 class="font-kisba text-4xl text-pink-950 duration-200 hover:p-1"><i>/75wæŋ/:</i></h1>
+		<h2 class="m-2.5 font-kisba text-2xl text-pink-900 duration-200 hover:p-1">
+			<b>75wang -</b> a customizable 75 hard tracker
+		</h2>
 	</div>
+
+	<!-- <input placeholder="enter goals here..." type="checkbox" /> -->
+	<form id="task-form" class="flex w-[40%] flex-row justify-between p-2" onsubmit={addTask}>
+		<input
+			type="text"
+			id="task-input"
+			bind:value={newTask}
+			bind:this={InputElement}
+			placeholder="enter goals here..."
+			class="mb-4 field-sizing-content p-2 px-6 font-hina text-[22px] text-pink-950 duration-300 hover:p-3 focus:outline-none"
+			autocomplete="off"
+		/>
+		<button
+			type="submit"
+			class="mb-4 ml-4 px-4 font-hina text-[22px] font-bold text-pink-800 duration-300 hover:scale-110 hover:rotate-10"
+			>+</button
+		>
+	</form>
+
+	<ul>
+		{#each tasks as task (task.id)}
+			<li>{task.text}</li>
+		{/each}
+	</ul>
+
+	<h3 class="font-hina text-xl text-pink-950">
+		i made this solely for myself but why not i guess anyone can use it
+	</h3>
 </div>
 
 <h3 class="m-2 px-4 text-left font-hina text-xl text-pink-950">
